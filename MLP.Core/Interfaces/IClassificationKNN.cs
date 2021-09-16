b0748049e6070be3561d82ce32036fc696a1e9e6
Zachary Teutsch <zteutsch@microsoft.com>
2021-09-16 13:03:26 -0700

    IClassificationKNN interface

--- MLP.Core/Interfaces/IClassificationKNN.cs
+++ MLP.Core/Interfaces/IClassificationKNN.cs
@@ -8,7 +8,7 @@ namespace MLP.Core.Interfaces
     // TODO
     public interface IClassificationKNN
     {
-
-
+        void Train(string x_feature, string y_feature, string target_feature);
+        string Classify(double x_data, double y_data);
     }
 }
