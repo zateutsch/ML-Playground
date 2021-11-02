a1ff784f562410bb1929df2819077753b2638354
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2021-11-01 23:47:00 -0700

    Real data added!

--- MLP.Core/Strings/InfoPaneStrings.cs
+++ MLP.Core/Strings/InfoPaneStrings.cs
@@ -21,7 +21,9 @@ namespace MLP.Core.Strings
         public static List<InfoItem> KMeansInfo = new List<InfoItem>(new[]
         {
             new InfoItem("Model Overview", "The K-Means Clustering model is another simple model that groups data into similar groups called \"clusters\". The model works with unlabeled data, and instead of guessing a label, it tries to sort data into segmented groups."),
-            new InfoItem("Adjusting the Model", "The main parameter we can work with here is the \"K\" in the model name. K represents how many different clusters we want to generate, and we can change this value to adjust how our data is seperated")
+            new InfoItem("Adjusting the Model", "The main parameter we can work with here is the \"K\" in the model name. K represents how many different clusters we want to generate, and we can change this value to adjust how our data is seperated"),
+            new InfoItem("Algorithm", "The K Means algorithm starts by picking random center points for each cluster. All of the data being clustered is then assigned to whichever \"centroid\" it is closests to. The new centroids are moved to the average of all the data points in the cluster, and the process is repeated until data stops getting reassigned."),
+            new InfoItem("Strengths", "K Means is great for getting insight from data that doesn't have labels and can't be easily classified. Separating data into clusters is a great way to look for revealing patterns in a data set.")
         });
     }
 }
